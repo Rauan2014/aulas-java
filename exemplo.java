7260575eb0fab1d5d45e4bda97e8dc005e9873ff
Rauan Bernardo Ribeiro de Araujo <rauanbernardo@gmail.com>
2024-04-24 13:15:40 -0300

    Add files via upload

---
+++ exemplo.java
@@ -0,0 +1,10 @@
+public class exemplo{
+    public static void main(String[] args){
+        String pais = "Brasil";
+        if (pais.equals("Brasil")) {
+            System.out.println("A string é igual a Brasil");
+        } else {
+            System.out.println("A String não é igual Brasil");
+        }
+    }
+}
